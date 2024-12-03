861bd0f408a5dc52098100dbe6f5401891eedef5
Samuel Wong <35249407+desktopofsamuel@users.noreply.github.com>
2024-12-03 09:29:11 +0800

    feat: :sparkles: Add Metadata API

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  POSTS_PER_PAGE: 6,
+  DATE_FORMAT: "LLLL d, yyyy",
+  DOMAIN: "https://juxtdesign.cc/",
+  DEFAULT_SOCIAL_IMAGE: "static/og.png",
+  SITE_TITLE: "JUXT DESIGN",
+  SEO_DESCRIPTION: "Discover the best design resources, figma template, articles for free. Make your design to the next level.",
+  SEO_KEYWORDS: "UX Design, Figma, Product Design, Design Community, UI, Sketch,CSS, Design",
+  X_HANDLE: "@juxtdesigncc",
+  X_ID: "1289809233488183296",
+}
